9e20e8b3e76870bbefabba498f3593910c1d3c95
Heru Susanto <herux@amg.co.id>
2020-02-14 17:23:49 +0700

    update callback

--- tests/minimal.lpr
+++ tests/minimal.lpr
@@ -11,6 +11,9 @@ begin
   cbCon := TCouchbaseConnection.Create;
   try
     cbCon.Connect('couchbase://localhost/testbucket', 'usercb', 'sa_karep_mu');
+    cbCon.Upsert('testMinimalKey', '{"ok1":"yes1"}');
+    cbCon.Add('testMinimalKeyAdd', '{"ok2":"yes2"}');
+    cbCon.Append('testMinimalKeyAdd', '{"ok3":"yes3"}');
   finally
     cbCon.Free;
   end;
--- tests/testcouchbase.pas
+++ tests/testcouchbase.pas
@@ -25,14 +25,22 @@ type
 implementation
 
 procedure TTestCouchbase.TestConnect;
+var
+  lValue: String;
+  cbRes: TCouchbaseResult;
 begin
   if not cbCon.Connect('couchbase://localhost/testbucket', 'usercb', 'sa_karep_mu') then
        Fail('Connection to couchbase failed, error: ' + cbCon.LastErrorDesc);
+
+  cbRes := cbCon.Upsert('test', '{"ok":"eko3"}');
+  WriteLn('success: ', cbRes.Success);
+  //cbCon.Get('afterTestConnect', lValue);
+  //WriteLn(lValue);
 end;
 
 procedure TTestCouchbase.TestUpsert;
 begin
-  //cbCon.Upsert('TestUpsert', '{"TestUpsert":"TestUpsert"}');
+  cbCon.Upsert('TestUpsert1', '{"TestUpsert":"TestUpsert"}');
 end;
 
 procedure TTestCouchbase.SetUp;
@@ -43,6 +51,7 @@ end;
 procedure TTestCouchbase.TearDown;
 begin
   cbCon.Free;
+  cbCon := Nil;
 end;
 
 initialization
