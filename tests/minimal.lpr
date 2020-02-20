8ae215d881a5f1156408798b3ebc76405aa112c8
Heru Susanto <herux@amg.co.id>
2020-02-20 17:15:58 +0700

    update testunit

--- tests/minimal.lpr
+++ tests/minimal.lpr
@@ -8,12 +8,12 @@ uses
 var
   cbCon: TCouchbaseConnection;
 begin
-  cbCon := TCouchbaseConnection.Create;
+  cbCon := TCouchbaseConnection.Create('couchbase://localhost/testbucket', 'usercb', 'sa_karep_mu');
   try
-    cbCon.Connect('couchbase://localhost/testbucket', 'usercb', 'sa_karep_mu');
+    cbCon.Connect;
     cbCon.Upsert('testMinimalKey', '{"ok1":"yes1"}');
-    cbCon.Add('testMinimalKeyAdd', '{"ok2":"yes2"}');
-    cbCon.Append('testMinimalKeyAdd', '{"ok3":"yes3"}');
+    //cbCon.Add('testMinimalKeyAdd', '{"ok2":"yes2"}');
+    //cbCon.Append('testMinimalKeyAdd', '{"ok3":"yes3"}');
   finally
     cbCon.Free;
   end;
--- tests/testcouchbase.pas
+++ tests/testcouchbase.pas
@@ -12,42 +12,57 @@ type
   { TTestCouchbase }
 
   TTestCouchbase= class(TTestCase)
-  private
-    cbCon: TCouchbaseConnection;
+  strict private
+    class var cbCon: TCouchbaseConnection;
+    class var FExampleJson: String;
+    class destructor Destroy;
+  public
+    class constructor Create;
   protected
     procedure SetUp; override;
     procedure TearDown; override;
   published
     procedure TestConnect;
-    //procedure TestUpsert;
+    procedure TestUpsert;
+    procedure TestUpsert2;
   end;
 
 implementation
 
 procedure TTestCouchbase.TestConnect;
-var
-  lValue: String;
-  cbRes: TCouchbaseResult;
 begin
   if not cbCon.Connect then
        Fail('Connection to couchbase failed, error: ' + cbCon.LastErrorDesc);
 end;
 
-//procedure TTestCouchbase.TestUpsert;
-//begin
-//  WriteLn('cbCon: ', cbCon <> nil);
-//  cbCon.Upsert('TestUpsert', '{"TestUpsert":"TestUpsert"}');
-//end;
+procedure TTestCouchbase.TestUpsert;
+begin
+  cbCon.Upsert('TestUpsert', '{"TestUpsert":"TestUpsert"}');
+end;
 
-procedure TTestCouchbase.SetUp;
+procedure TTestCouchbase.TestUpsert2;
+begin
+  cbCon.Upsert('TestUpsert2', '{"TestUpsert":"TestUpsert"}');
+end;
+
+class destructor TTestCouchbase.Destroy;
+begin
+  cbCon.Free;
+end;
+
+class constructor TTestCouchbase.Create;
 begin
   cbCon := TCouchbaseConnection.Create('couchbase://localhost/testbucket', 'usercb', 'sa_karep_mu');
 end;
 
+procedure TTestCouchbase.SetUp;
+begin
+
+end;
+
 procedure TTestCouchbase.TearDown;
 begin
-  cbCon.Free;
-  cbCon := Nil;
+
 end;
 
 initialization
